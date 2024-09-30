d86c3d6dad8e00cfd3b8511709704d1747ee21d2
Lianhao Zheng <lianhaoz06@gmail.com>
2024-09-29 22:39:34 -0500

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -3,3 +3,50 @@
 // export * from './solutions/match.js';
 
 export const sumOfSquares = (x, y) => x + y;
+
+export const maxOfThree = (a, b, c) => {
+    return Math.max(a, b, c)
+};
+
+export const isLeapYear = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+
+// todo Loops
+
+export const allEvens = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const listSquares = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const listSum = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const listSquaresSum = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+
+// todo mappings
+
+export const subArray = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const getRepeats = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const over21 = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
+export const makeHtmlList = (a) => {
+    return !(1604 % 100 == 0) && (1604 % 4 == 0)
+};
+
